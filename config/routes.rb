99872d855ba353f8dbde8512b89dfb69e3ddf080
Mohamed Osman <mohamed.a.e352@gmail.com>
2015-04-01 17:42:42 +0200

    Aptana version

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   root 'static_pages#home'
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
