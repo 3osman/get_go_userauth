fca54eafc9c07250e6b0cbbb8041e09665944846
Mohamed Osman <mohamed.a.e352@gmail.com>
2015-04-03 14:34:34 +0200

    Icons

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.raise_runtime_errors = true
   config.assets.compile = true
   config.assets.enabled = true
   config.assets.paths << Rails.root.join('/app/assets/fonts')
