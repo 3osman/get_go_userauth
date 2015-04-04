2afe3dbd16a9543398499e59aa732fc80b0f4594
Mohamed Osman <mohamed.a.e352@gmail.com>
2015-04-04 12:44:43 +0200

    Reiteration of assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,93 +13,16 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require twitter/bootstrap
-//= require bootstrap-select
-//= require bootstrap-switch
-//= require_tree .
-
-(function($) {
-    // Switch
-	$("[data-toggle='switch']").bootstrapSwitch({baseClass: 'switch'});
-
-	$('#top-toolbar').toolbar({
-		content: '#user-toolbar-options',
-		position: 'top'
-	});
-
-	$('#bottom-toolbar').toolbar({
-		content: '#user-toolbar-options',
-		position: 'bottom'
-	});
-
-	$('#vertical-top-toolbar').toolbar({
-		content: '#user-toolbar-options',
-		position: 'vertical-top'
-	});
-
-	$('#vertical-bottom-toolbar').toolbar({
-		content: '#user-toolbar-options',
-		position: 'vertical-bottom'
-	});
-
-})(jQuery);
-
-// Bootstrap input select
-$('.selectpicker').selectpicker();
-
-
-// Roadmap finish subtask
-$('.panel-title-btn').click(function(event) {
-	event.stopPropagation();
-	var parent = $(this).parent();
-	if (parent.hasClass('done')) {
-		parent.removeClass('done');
-	} else {
-		parent.addClass('done');
-	}
-	event.preventDefault();
-});
-
-// Roadmap adding personal comment
-
-//Hide all comment fields
-$('.add-comment').hide();
-
-//Add comment
-$('.add-comment-btn').click(function(e) {
-	var parent = $(this).parent();
-	parent.find('.add-comment').show();
-	$(this).hide();
-	e.preventDefault();
-});
-
-//Save comment
-$('.comment-save').click(function(event) {
-	var parent = $(this).parent();
-	var comments_container = parent.parent().find('.comments-container');
-	comments_container.append('\
-		<div class="alert alert-warning alert-dismissable user-comment">\
-		<button type="button" class="close fa-times fa" data-dismiss="alert" aria-label="Close"></button>\
-        ' + parent.find('.form-control').val() + '</div>');
-	parent.parent().find('.form-control').val('');
-	parent.parent().find('.add-comment-btn').show();
-	parent.hide();
-});
-
-//Cancel
-$('.comment-cancel').click(function() {
-	var parent = $(this).parent();
-	parent.hide();
-	parent.parent().find('.form-control').val('');
-	parent.parent().find('.add-comment-btn').show();
-
-});
-
-
-
-
-
-
+//= require jquery-1.11.2.min.js
+//= require bootstrap.min.js
+//= require jasny-bootstrap.js
+//= require checkbox.js
+//= require radio.js
+//= require bootstrap-switch.js
+//= require toolbar.js
+//= require starrr.js
+//= require bootstrap-select.min.js
+//= require application.js
 
 
 
