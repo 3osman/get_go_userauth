31857dadc9d2d06742a58afeea6cd9a9c89db85d
Mohamed Osman <mohamed.a.e352@gmail.com>
2015-04-05 15:20:25 +0200

    Email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-              :omniauthable, :omniauth_providers => [:facebook]
+              :omniauthable,:confirmable, :omniauth_providers => [:facebook]
   validates :name, presence: true
   validates :country, presence: true
   validates :university, presence: true
